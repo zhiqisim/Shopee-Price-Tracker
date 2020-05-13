381cbd5dc7ab85c634b0ab5cfaedb5b5be74a9d8
Sim Zhi Qi <zhiqi.sim@C02C979SMD6M.local>
2020-05-13 16:53:40 +0800

    add in API-gateway with Go and connect Nginx Reverse Proxy

---
+++ api-gateway/src/api.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/gin-gonic/gin"
+// import "net/http"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello world",
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+
